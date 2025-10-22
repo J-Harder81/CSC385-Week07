5c3440f02131d24568c95a78f093c4ec18b24a15
J-Harder81 <justin.a.harder@outlook.com>
2025-10-22 01:30:09 -0600

    Added source code files

---
+++ prod-cons.c
@@ -0,0 +1,73 @@
+/*
+ * This program simulates the producer-consumer problem using POSIX mutex and semaphore.
+ */
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h> // included for sleep() function
+#define MAX 5
+#define BUFFSIZE 5
+sem_t empty;
+sem_t full;
+int buffer[BUFFSIZE];
+pthread_mutex_t mutex;
+int in = 0;
+int out = 0;
+int item = 0;
+
+void *producer(void *p)
+{   
+    for(int i = 0; i < MAX; i++) {
+	item = rand() %RAND_MAX; // produce random integer to be inserted into the buffer
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+        buffer[in] = item;
+        // item = item + 1; commented out for item using random integer
+        printf("producer %d: insert item %d at\tbuffer[%d]\n", *((int *)p), buffer[in], in);
+        in = (in+1) % BUFFSIZE;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+	sleep(rand() %5); // producer sleep for a random period of time between 0 and 5 seconds
+    }
+}
+
+void *consumer(void *c)
+{   
+    for(int i = 0; i < MAX; i++) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+        int item_removed = buffer[out];
+        printf("consumer %d: remove item %d from buffer[%d]\n",*((int *)c), item_removed, out);
+        out = (out+1) % BUFFSIZE;
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+	sleep(rand() %5); // consumer sleep for a random period of time between 0 and 5 seconds
+    }
+}
+
+int main()
+{   
+    pthread_t p[5],c[5];
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, BUFFSIZE);
+    sem_init(&full,0,0);
+    int id;
+    int a[5] = {1,2,3,4,5}; //Keep track of the producer and consumer
+    for(int i = 0; i < 5; i++) {
+        pthread_create(&p[i], NULL, (void *)producer, (void *)&a[i]);
+    }
+    for(int i = 0; i < 5; i++) {
+        pthread_create(&c[i], NULL, (void *)consumer, (void *)&a[i]);
+    }
+    for(int i = 0; i < 5; i++) {
+        pthread_join(p[i], NULL);
+    }
+    for(int i = 0; i < 5; i++) {
+        pthread_join(c[i], NULL);
+    }
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    return 0;   
+}
